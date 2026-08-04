fe7e1c8cbfd712256e9767501e1c23392255380b
syedbaji8 <syedbaji8@gmail.com>
2026-08-04 03:06:12 -0400

    contact page addedd

--- health_contact.js
+++ health_contact.js
@@ -5,7 +5,7 @@ function thankyou() {
     const condition = document.getElementById('condition').value;
     const message = document.getElementById('message').value;
     try {
-        if (name === '' && email === '' && condition === '' && message === '') {
+        if (name === '' || email === '' || condition === '' || message === '') {
             throw new Error('Please fill mandatory inputs!')
         }
         alert("Thank you for contacting us!");
